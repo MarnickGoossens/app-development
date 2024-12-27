cee39d6b5f35794f297663f6c645a47d5303f013
Marnick Goossens <r0930150@student.thomasmore.be>
2024-12-27 17:08:33 +0100

    get users

---
+++ lib/apis/api.dart
@@ -0,0 +1,79 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import '../models/user.dart';
+
+class UserApi {
+  static String server = 'localhost:3000';
+
+  static Future<List<User>> fetchUsers() async {
+    var url = Uri.http(server, '/users');
+
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      List jsonResponse = json.decode(response.body);
+      return jsonResponse.map((user) => User.fromJson(user)).toList();
+    } else {
+      throw Exception('Failed to load users');
+    }
+  }
+
+  static Future<User> fetchUser(int id) async {
+    var url = Uri.https(server, '/users/$id');
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to load user');
+    }
+  }
+
+  static Future<User> createUser(User user) async {
+    var url = Uri.https(server, '/users');
+
+    final http.Response response = await http.post(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(user),
+    );
+    if (response.statusCode == 201) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create user');
+    }
+  }
+
+  static Future<User> updateUser(int id, User user) async {
+    var url = Uri.https(server, '/users/$id');
+
+    final http.Response response = await http.put(
+      url,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(user),
+    );
+    if (response.statusCode == 200) {
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to update user');
+    }
+  }
+
+  static Future deleteUser(int id) async {
+    var url = Uri.https(server, '/users/$id');
+
+    final http.Response response = await http.delete(url);
+    if (response.statusCode == 200) {
+      return;
+    } else {
+      throw Exception('Failed to delete user');
+    }
+  }
+}
---
+++ lib/pages/inlog_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import '../apis/api.dart';
+import '../models/user.dart';
+
+class InlogPage extends StatefulWidget {
+  const InlogPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _InlogPageState();
+}
+
+class _InlogPageState extends State {
+  List<User> userList = [];
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _getUsers();
+  }
+
+  void _getUsers() {
+    UserApi.fetchUsers().then((result) {
+      setState(() {
+        userList = result;
+        count = result.length;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Users"),
+        backgroundColor: Colors.red,
+        foregroundColor: Colors.white,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(5.0),
+        child: _userListItems(),
+      ),
+    );
+  }
+
+  ListView _userListItems() {
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.red,
+              child: Text(userList[position].name.substring(0, 2)),
+            ),
+            title: Text(userList[position].name),
+            subtitle: const Text("subtitle"),
+          ),
+        );
+      },
+    );
+  }
+}
